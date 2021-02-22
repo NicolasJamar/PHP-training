0b4645b2eb5e3ebeb5e70f4419438a1dde64c7f0
NicolasJamar <nicolas.jamar@gmail.com>
2021-02-22 14:22:22 +0100

    add collection

--- src/classes/recipecollection.php
+++ src/classes/recipecollection.php
@@ -4,4 +4,29 @@ class RecipeCollection
 {
 	private $title;
 	private $recipes = [];
+
+	public function __construct($title)
+	{
+		$this->setTitle($title);	
+	}
+
+	public function setTitle($title) 
+	{
+		$this->title = ucwords($title);
+	}
+
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	public function addRecipe($recipe)
+	{
+		$this->recipes[] = ucwords($recipe);
+	}
+
+	public function getRecipes()
+	{
+		return $this->recipes;
+	}
 }
