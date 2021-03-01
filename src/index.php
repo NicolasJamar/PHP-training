a394adc84978da986f6f73b551c3611b6b7cefcb
NicolasJamar <nicolas.jamar@gmail.com>
2021-03-01 23:01:40 +0100

    recipe collection

--- src/index.php
+++ src/index.php
@@ -5,6 +5,15 @@ include "classes/render.php";
 include "classes/recipecollection.php";
 include "inc/cookbook.php";
 
-echo Render::displayRecipe($recipe1);
+// echo Render::displayRecipe($recipe1);
+$cookbook = new RecipeCollection("Becode recipe");
+$cookbook->addRecipe($lemon_chicken);
+$cookbook->addRecipe($granola_muffins);
+$cookbook->addRecipe($belgian_waffles);
+$cookbook->addRecipe($pepper_casserole);
+$cookbook->addRecipe($lasagna);
+$cookbook->addRecipe($dried_mushroom_ragout);
+
+var_dump($cookbook);
 	
 ?>
